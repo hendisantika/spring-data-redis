2361b96c9f51f80ebe7659d485bc92bc0a28f7d9
Hendi Santika <hendisantika@gmail.com>
2017-12-22 09:19:32 +0700

    #7 - Add Post Exception Handler

---
+++ src/main/java/com/hendisantika/springdataredis/controller/PostExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springdataredis.controller;
+
+import com.hendisantika.springdataredis.exception.PostNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-redis
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/12/17
+ * Time: 09.18
+ * To change this template use File | Settings | File Templates.
+ */
+
+@ControllerAdvice
+public class PostExceptionHandler {
+    private Logger log = LoggerFactory.getLogger(PostExceptionHandler.class);
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Post Not Found")
+    @ExceptionHandler(PostNotFoundException.class)
+    public void handlePostNotFound(HttpServletRequest request, Exception ex) {
+        log.error("{} : {}", ex.getMessage(), request.getRequestURI());
+    }
+}
